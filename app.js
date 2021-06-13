3d0eb7f19aeb6ace12d4993a4975063b0b0bb28d
Artem <68990883+Artem9021@users.noreply.github.com>
2021-06-13 11:19:20 +0300

    Update: option to exclude common words
    Option to exclude common words
    Added bars for better visualization
    Layout fixes
    Added band logo

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+/* https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers */
 
 window.addEventListener("load", () => {
     let text = document.querySelector("textarea").value.toLowerCase().replace(/['!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~']/g," ").replace(/\s{2,}/g," ").replace(/(\r\n|\n|\r)/gm, " ");
@@ -7,8 +12,9 @@ window.addEventListener("load", () => {
     text = text.split(" ")
     
     let words = {}
-    let common = ["t", "s", "ll", "m", "re", "ve", "x2", "o", "x3", "x4", "x5", "pre", "chorus", "u", "2"]
-    // i originally wanted to remove 100 most common words, but ended up just removing worlds like I'm and I've for more fair results
+    let removeCompletely = ["t", "s", "ll", "m", "re", "ve", "x2", "o", "x3", "x4", "x5", "pre", "chorus", "u", "2", "don", "d"]
+    
+    let commonWords = ["the", "of", "to", "and", "a", "in", "is", "it", "you", "that", "he", "was", "for", "on", "are", "with", "as", "i", "his", "they", "be", "at", "one", "have", "this", "from", "or", "had", "by", "not", "word", "but", "what", "some", "we", "can", "out", "other", "were", "all", "me", "your", "our", "he", "she", "my", "so", "go", "am", "will", "no", "an", "oh", "did", "got", "has", "do", "well", "if", "mine", "why", "get"]
 
 
     text.forEach(word => {
@@ -16,7 +22,6 @@ window.addEventListener("load", () => {
     	
 
     	words[word] += 1;
-    	//console.log(isNaN(words[word]))
 
     	if (isNaN(words[word])){
     		words[word] = 1;
@@ -27,37 +32,61 @@ window.addEventListener("load", () => {
 
 
 var sortable = [];
-for (var word in words) {
-	if (!common.includes(word)) {
-    	sortable.push([word, words[word]]);	
+
+showCommon = 0;
+let common = document.querySelector(".common")
+common.addEventListener("input", ()=>{
+    showCommon = !showCommon
+    renderList();
+})
+
+const renderList = () => {
+    sortable = []
+    for (var word in words) {
+    if ((!removeCompletely.includes(word) && !commonWords.includes(word)) || (!removeCompletely.includes(word) && showCommon)) {
+        sortable.push([word, words[word]]); 
     }
     
 }
 
-sortable.sort(function(a, b) {
-	
 
+
+
+sortable.sort(function(a, b) {
     return b[1] - a[1];
 });
     
-    console.log(sortable)
+    
 
     const list = document.querySelector(".word-list")
+    list.innerText = ""
+
     sortable.forEach(word => {
-    	let item = document.createElement("li")
-    	let wordelem = document.createElement("span")
-    	let countelem = document.createElement("span")
-    	countelem.classList.add("badge")
+        let item = document.createElement("li")
+        item.classList.add("item")
+        max = sortable[0][1]
+        item.style.width = `${scale(word[1], 1, max, 1, 90)}%`
+        
+        let wordelem = document.createElement("span")
+        let countelem = document.createElement("span")
+        countelem.classList.add("badge")
 
-    	wordelem.innerText = word[0]
-    	countelem.innerText = word[1]
-    	
-    	item.appendChild(wordelem)
-    	item.appendChild(countelem)
+        wordelem.innerText = word[0]
+        countelem.innerText = word[1]
+        
+        item.appendChild(wordelem)
+        item.appendChild(countelem)
 
-    	list.appendChild(item)
+        list.appendChild(item)
 
     })
+
+}
+
+renderList();
 })
+
+
+
 
 
